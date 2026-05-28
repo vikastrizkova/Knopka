d437d7e7e71182d06ec654638873ecaed265972f
Виктория <vikastrizkova@gmail.com>
2026-05-28 16:44:20 +0300

    Исправлена синтаксическая ошибка

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ let s = 'не в руке';
 http.createServer((req, res) => {
     if (req.url.includes('knopka=on')) s = 'в руке';
     if (req.url.includes('knopka=off')) s = 'не в руке';
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
     res.end(`<h1>Состояние: ${s}</h1><a href="?knopka=on">ON</a> | <a href="?knopka=off">OFF</a>`);
 }).listen(process.env.PORT || 3000);
